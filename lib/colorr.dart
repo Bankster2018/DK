c991685b40d4bcabc92ef0d0f4bdfd8272a60c3f
Bankster2018 <bankster2018@gmail.com>
2021-05-20 22:10:30 -0400

    intial commit

---
+++ lib/colorr.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF6F35A5);
+const kPrimaryLightColor = Color(0xFF8535EA);
+const btnforGroundColr = Color(0xff331074);
+const btnbackGroundColr = Color(0x5A41D0);
